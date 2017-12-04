3127b992194d0525eb1f8c068a497a9f6505783c
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-12-04 16:32:25 -0500

    Added templates

---
+++ 01.01_templates.py
@@ -0,0 +1,4 @@
+# Templates contain the static parts of the HTML page (like a scaffolding)
+
+# Template tags (Django Template Variables) have their own special syntax
+# This syntax allows you to inject dynamic content that your Django app's views will produce, effecting the final HTML
