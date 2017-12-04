08c54adec4092bfe9b1209c26182729310538ff6
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-12-04 15:53:11 -0500

    First project

---
+++ 00.00_whatIsDjango.py
@@ -0,0 +1,13 @@
+# Django is a free and open source web framework
+# It is used by many sites, including Pinterest, PBS, INstagram, BitBucket, Washington Times, Mozilla, etc.
+
+# Allows us to solve two major problems:
+# Map a requested URL from user to the code that's actually meant to handle it
+# Create requested HTML dynamically
+
+# Using templates, we can actually inject from a database to a page for the user
+
+# Because the original developers were surrounded by writers (created in newspaper), good written documentation is a key part of Django
+# This means you have excellent references to check on the official Django docs
+
+# USE VIRTUAL ENVIRONMENTS!!!
