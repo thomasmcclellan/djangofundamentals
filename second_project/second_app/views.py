a412e15609aa908d0d813b8af571f39c91a149f0
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-12-05 15:19:52 -0500

    Added model forms

--- second_project/second_app/views.py
+++ second_project/second_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from second_app.models import User
+# from second_app.models import User
+from second_app.forms import NewUserForm
 
 # Create your views here.
 
@@ -13,6 +14,18 @@ def index(request):
 #   return render(request,'second_app/help.html',context=help_dict)
 
 def users(request):
-  user_list = User.objects.order_by('first_name')
-  user_dict = {'users':user_list}
-  return render(request,'second_app/users.html',context=user_dict)
+  # user_list = User.objects.order_by('first_name')
+  # user_dict = {'users':user_list}
+  # return render(request,'second_app/users.html',context=user_dict)
+   
+  form = NewUserForm()
+
+  if request.method == 'POST':
+    form = NewUserForm(request.POST)
+
+    if form.is_valid():
+      form.save(commit=True)
+      return index(request)
+    else:
+      print('ERROR, FORM INVALID')
+  return render(request,'second_app/users.html',{'form':form})
