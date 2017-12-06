444326ac1ad7a113f17e392f384da4f962e08f86
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-12-06 12:15:16 -0500

    Added template inheritances

---
+++ 01.03_templates_templateInheritance_DRY.py
@@ -0,0 +1,4 @@
+# DRY => Don't Repeat Yourself
+# Template Inheritance allows us to create a base template we can inherit from 
+# This saves us a ot of repetitive work and makes it much easier to maintain the same base look and feel across our entire website
+# i.e. => navbars 
