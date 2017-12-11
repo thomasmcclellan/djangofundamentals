d21b54228ac7d1bd680aef364e01633dd1085ddd
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-12-11 10:09:48 -0500

    Added comment and publish functionality

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
   url(r'^about/$',views.AboutView.as_view(),name='about'),
   url(r'^post/(?p<pk>\d+)$',views.PostDetailView.as_view(),name='post_detail'),
   url(r'^post/new/$',views.CreatePostView.as_view(),name='post_new'),
-  url(r'^post/(?p<pk>\d+)$/edit/$',views.PostUpdateView.as_view(),name='post_edit'),
-  url(r'^post/(?p<pk>\d+)$/remove/$',views.PostDeleteView.as_view(),name='post_remove'),
+  url(r'^post/(?p<pk>\d+)/edit/$',views.PostUpdateView.as_view(),name='post_edit'),
+  url(r'^post/(?p<pk>\d+)/remove/$',views.PostDeleteView.as_view(),name='post_remove'),
   url(r'^drafts/$',views.DraftListView.as_view(),name='post_draft_list'),
+  url(r'^post/(?p<pk>\d+)/comment/$',views.add_comment_to_post,name='add_comment_to_post'),
+  url(r'^comment/(?p<pk>\d+)/approve/$',views.comment_approve,name='comment_approve'),
+  url(r'^comment/(?p<pk>\d+)/remove/$',views.comment_remove,name='comment_remove'),
+  url(r'^post/(?p<pk>\d+)/publish/$',views.post_publish,name='post_publish'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
 from .models import Post,Comment
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
@@ -46,3 +47,37 @@ class DraftListView(LoginRequiredMixin,ListView):
 
   def get_queryset(self):
     return post.objects.filter(published_date__isnull=True).order_by('-created_date')
+
+
+@login_required
+def add_comment_to_post(reqest,pk):
+  post = get_object_or_404(Post,pk=pk)
+  if request.method == 'POST':
+    from = CommentForm(request.POST)
+    if form.is_valid():
+      comment = form.save(commit=False)
+      commit.post = post
+      commit.save()
+      return redirect('post_detail',pk=post.pk)
+  else:
+    form = CommentForm()
+  return render(request,'blog/comment_form.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+  comment = get_object_or_404(Comment,pk=pk)
+  comment.approve()
+  return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(reqest,pk):
+  comment = get_object_or_404(Comment,pk=pk)
+  post_pk = comment.post.pk 
+  comment.delete()
+  return redirect('post_detail',pk=post_pk)
+
+@login_required
+def post_publish(request,pk):
+  post = get_object_or_404(Post,pk=pk)
+  post.publish
+  return redirect('post_detail',pk=pk)
