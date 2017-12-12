a285bba433638abba595844ad4d0fce19432ac96
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-12-12 11:18:06 -0500

    Infilled groups

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
+from .models import Group,GroupMember
 # Create your views here.
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+  fields = ('name','description')
+  model = Group
+
+class SingleGroup(generic.DetailView):
+  model = Group
+
+class ListGroup(generic.ListGroup):
+  model = Group
